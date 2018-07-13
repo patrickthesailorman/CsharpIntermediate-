196469312d4de067237df1fc77a372f2e6205750
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-13 19:23:34 -0400

    Add

--- section2/Customer.cs
+++ section2/Customer.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Collections.Generic;
+
 namespace section2 {
   public class Customer {
     public int Id;
     public string Name;
+    public List<Order> Orders;
     public Customer()
-    {
-        
+    {    
+        Orders = new List<Order>();
     }
     public Customer(int id)
     {
