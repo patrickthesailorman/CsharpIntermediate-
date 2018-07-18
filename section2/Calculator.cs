e8988f94706c3c4fb242bb0f271813c60d078a21
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-17 20:52:06 -0400

    Add Calculator

---
+++ section2/Calculator.cs
@@ -0,0 +1,15 @@
+namespace section2
+{
+  public class Calculator
+  {
+    public int Add(params int[] numbers)
+    {
+      var sum = 0;
+      foreach(var number in numbers)
+      {
+        sum += number;
+      }
+      return sum;
+    }
+  }
+}
