55344fc5b1e727432dc2914b627e2ae6d9fbbfa5
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-18 16:38:38 -0400

    Add Program

--- section2/firstFiles/Program.cs
+++ section2/firstFiles/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace firstFiles
+namespace section2
 {
-    class Program
+  class Program 
+  {
+    static void Main (string[] args) 
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      // var customer = new Customer (1, "John");
+
+      // var order = new Order();
+      // customer.Orders = new List<Order>();
+      // customer.Orders.Add(order);
+
+      // Console.WriteLine (customer.Id);
+      // Console.WriteLine (customer.Name);
+      try
+      {
+          var num = int.Parse("abc");
+      }
+      catch(Exception)
+      {
+          Console.WriteLine("Conversion failed");
+      }
+      int number;
+      var result = int.TryParse("abc", out number);
+      if (result)
+        Console.WriteLine(number);
+      else
+        Console.WriteLine("Conversion failed");
     }
+    static void UseParams()
+    {
+      var calculator = new Calculator();
+      Console.WriteLine(calculator.Add(1, 2));
+      Console.WriteLine(calculator.Add(1, 2, 3));
+      Console.WriteLine(calculator.Add(1, 2, 3, 4));
+      Console.WriteLine(calculator.Add(new int[]{1, 2, 3, 4, 5, 6}));
+    }
+    static void UsePoints()
+    {
+      try
+      {
+        var point = new Point(10,20);
+        point.Move(null);
+        Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+        point.Move(100, 200);
+        Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+      }
+      catch(Exception)
+      {
+          Console.WriteLine("An unexpected error occured");
+      }
+    }
+  }
 }
