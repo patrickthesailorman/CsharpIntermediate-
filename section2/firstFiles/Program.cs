bd4a4100461746dfd1955989c908dd51bbf27601
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-18 16:36:53 -0400

    Add firstFiles

---
+++ section2/firstFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
