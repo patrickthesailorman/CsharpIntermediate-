b10829f5d99623c708c8eec58e9f7fa6fb13e313
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-20 21:47:04 -0400

    Add Customer

--- section2/fields/Program.cs
+++ section2/fields/Program.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace fields
 {
+    public class Customer {
+    public int Id;
+    public string Name;
+    public List<Order> Orders;
+    }
+    public class Order 
+    {
+        
+    }
     class Program
     {
         static void Main(string[] args)
