85da74aaeb3b02fdea9b3913d8a276d3631d8dba
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-22 18:21:01 -0400

    Console Birthdate

--- section2/Encapsulation/Program.cs
+++ section2/Encapsulation/Program.cs
@@ -22,6 +22,7 @@ namespace Encapsulation
         {
              var person = new Person();
              person.SetBirthdate(new DateTime(1982, 1, 1));
+             Console.WriteLine(person.GetBirthdate());
         }
     }
 }
