052b48c67c37ca4c8e09b1d6f5feca276e0cc927
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-22 15:01:23 -0400

    Add Customer.cs

---
+++ section2/fields/Customer.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace fields
+{
+  public class Customer {
+    public int Id;
+    public string Name;
+    public List<Order> Orders = new List<Order>();
+    
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+        public void Promote()
+        {
+            Orders = new List<Order>();
+        }
+  }
+}
