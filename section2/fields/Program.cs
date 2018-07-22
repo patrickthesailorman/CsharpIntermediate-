a5a2a1324ca63853fc9470fdad237cc7b756090a
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-22 15:04:08 -0400

    Add new Customer

--- section2/fields/Program.cs
+++ section2/fields/Program.cs
@@ -3,34 +3,13 @@ using System.Collections.Generic;
 
 namespace fields
 {
-    public class Customer {
-    public int Id;
-    public string Name;
-    public List<Order> Orders = new List<Order>();
-    
-        public Customer(int id)
-        {
-            this.Id = id;
-        }
-        public Customer(int id, string name)
-            : this(id)
-        {
-            this.Name = name;
-        }
-        public void Promote()
-        {
-            Orders = new List<Order>();
-        }
-}
-    public class Order 
-    {
-
-    }
-    class Program
+  class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customer = new Customer(1);
+            
+            // Console.WriteLine("Hello World!");
         }
     }
 }
