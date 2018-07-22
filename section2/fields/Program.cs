ddeec6896ec37f68c951fb35ceca80a97c1ca976
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-22 15:05:44 -0400

    Add Order

--- section2/fields/Program.cs
+++ section2/fields/Program.cs
@@ -8,7 +8,7 @@ namespace fields
         static void Main(string[] args)
         {
             var customer = new Customer(1);
-            
+            customer.Orders.Add(new Order());
             // Console.WriteLine("Hello World!");
         }
     }
