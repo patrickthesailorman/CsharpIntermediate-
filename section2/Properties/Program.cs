dc2f00b3529105ef3268411daf03f1748b1956d2
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-23 17:02:07 -0400

    Add Person

--- section2/Properties/Program.cs
+++ section2/Properties/Program.cs
@@ -2,11 +2,15 @@
 
 namespace Properties
 {
+    public class Person
+    {
+        public DateTime Birthdate { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
         }
     }
 }
