f526901e71bee518106b5e5c6314715b35994905
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-24 17:46:06 -0400

    Add timeSpan

--- section2/Properties/Program.cs
+++ section2/Properties/Program.cs
@@ -5,7 +5,16 @@ namespace Properties
     public class Person
     {
         public DateTime Birthdate { get; set; }
+         public int Age
+         {
+             get
+             {
+                 var timeSpan = DateTime.Today - Birthdate;
+                 timeSpan.Days
+             }
+         }
     }
+   
     class Program
     {
         static void Main(string[] args)
