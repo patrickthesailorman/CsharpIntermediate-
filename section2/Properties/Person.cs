3a111236345af298210482ae62cc2778fceef589
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-25 18:50:31 -0400

    Move props to top

--- section2/Properties/Person.cs
+++ section2/Properties/Person.cs
@@ -7,11 +7,13 @@ namespace Properties
     {
       public string Name { get; set; }
       public string Username { get; set; }
+      public DateTime Birthdate { get; private set; }
+      
       public Person(DateTime birthdate)
       {
           Birthdate = birthdate;
       }
-        public DateTime Birthdate { get; private set; }
+        
          public int Age
          {
              get
