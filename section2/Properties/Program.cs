0b6579c5bc48ccef844d1c25ff13e1c4f5365866
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-25 18:38:14 -0400

    Add new birthdate

--- section2/Properties/Program.cs
+++ section2/Properties/Program.cs
@@ -8,8 +8,7 @@ namespace Properties
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.Birthdate = new DateTime(1982, 1, 1);
+            var person = new Person(new DateTime(1982, 1, 1));
 
             Console.WriteLine(person.Age);
 
