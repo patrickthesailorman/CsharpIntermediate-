701f0b0ff041ef3c950f58d8890ee5675ddbe814
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-25 22:17:50 -0400

    Add HttpCookie

--- section2/Indexers/Program.cs
+++ section2/Indexers/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Indexers
 {
     class Program
     {
+        public class HttpCookie
+        {
+            
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
