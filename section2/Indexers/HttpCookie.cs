9ec3bf477d686512f548d590547d0b373dce5eb9
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-26 11:22:30 -0400

    Add key value

--- section2/Indexers/HttpCookie.cs
+++ section2/Indexers/HttpCookie.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 
 namespace Indexers
 {
-  partial class Program
-    {
-    public class HttpCookie
-        {
-            private Dictionary<string, string>
-        }
-    }
+  public class HttpCookie
+  {
+      private Dictionary<string, string> _dictionary;
+      public HttpCookie()
+      {
+        _dictionary = new Dictionary<string, string>();
+          
+      }
+      public string this[string key]
+      {
+        get {return _dictionary[key]; }
+        set { _dictionary[key] = value; }
+
+      }
+  }
 }
