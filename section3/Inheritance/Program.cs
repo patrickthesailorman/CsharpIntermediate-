76d872c046c3562acd60c921659f1658abaef46b
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-28 18:49:18 -0400

    Add Copy method

--- section3/Inheritance/Program.cs
+++ section3/Inheritance/Program.cs
@@ -6,6 +6,11 @@ namespace Inheritance
     {
         public int Width { get; set; }
         public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard.");
+        }
     }
     class Program
     {
