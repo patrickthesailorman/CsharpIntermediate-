7aa1b4cbc511cdab8b4df90963953fc314ffd21b
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-27 20:32:04 -0400

    Add stopwatch

--- section2/stopwatch/Program.cs
+++ section2/stopwatch/Program.cs
@@ -4,6 +4,18 @@ namespace stopwatch
 {
     class Program
     {
+        public class timer
+        {
+            var startTime = DateTime.Now;
+            var stopTime = DateTime.Now;
+
+            var timespan = stopTime - startTime;
+       
+            public int Time
+            {
+
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
