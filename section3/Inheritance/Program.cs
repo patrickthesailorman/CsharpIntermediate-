3aa04fbfbff10bbcd80aec17a37706712c994bd6
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-28 21:15:52 -0400

    Add FontName

--- section3/Inheritance/Program.cs
+++ section3/Inheritance/Program.cs
@@ -5,7 +5,7 @@ namespace Inheritance
     public class Text : PresentationObject
     {
         public int FontSize { get; set }
-        
+        public string FontName { get; set; }
     }
     class Program
     {
