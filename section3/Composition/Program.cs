a12d9f430664ab3f097cb98a2f5b39a39dedcd94
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-29 12:57:57 -0400

    Add Installer()

--- section3/Composition/Program.cs
+++ section3/Composition/Program.cs
@@ -4,9 +4,16 @@ namespace Composition
 {
     public class Installer
     {
+        private readonly Logger _logger;
+
         public Installer(Logger logger)
         {
             _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are installing the application.");
         }
     }
     class Program
