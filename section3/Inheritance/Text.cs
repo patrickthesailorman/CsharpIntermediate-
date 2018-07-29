fc929811541d3aef8ea0d4200bf2411aa450fa06
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-28 21:18:55 -0400

    Add Text.cs

---
+++ section3/Inheritance/Text.cs
@@ -0,0 +1,13 @@
+namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to " + url );
+        }
+    }
+}
