4ae5835c6a08d7398052b332a06e58e80e8654ac
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-28 21:13:10 -0400

    Add Text :

--- section3/Inheritance/Program.cs
+++ section3/Inheritance/Program.cs
@@ -2,20 +2,9 @@
 
 namespace Inheritance
 {
-    public class PresentationObject
+    public class Text : PresentationObject
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
-
-        public void Copy()
-        {
-            Console.WriteLine("Object copied to clipboard.");
-        }
-
-        public void Duplicate()
-        {
-            Console.WriteLine("Object was duplicated.");
-        }
+        
     }
     class Program
     {
