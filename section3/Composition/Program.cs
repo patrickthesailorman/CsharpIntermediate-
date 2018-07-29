3d4cfdecd5b4a2635550711fd05b11459700c38f
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-29 13:11:19 -0400

    Add Program

--- section3/Composition/Program.cs
+++ section3/Composition/Program.cs
@@ -2,25 +2,19 @@
 
 namespace Composition
 {
-    public class Installer
-    {
-        private readonly Logger _logger;
-
-        public Installer(Logger logger)
-        {
-            _logger = logger;
-        }
-
-        public void Install()
-        {
-            _logger.Log("We are installing the application.");
-        }
-    }
+   
     class Program
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World!");
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
         }
     }
 }
