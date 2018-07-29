e6b12904fb5ed2f4976d726b9148ad64a9e1e5f1
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-29 11:56:45 -0400

    Add Logger

--- section3/Composition/Program.cs
+++ section3/Composition/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Composition
 {
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
         }
     }
 }
