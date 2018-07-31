ecb6934630ed2283dfa3700068a3628d3fb42f59
patrickthesailorman <patrickthesailorman@gmail.com>
2018-07-31 19:55:28 -0400

    Add Calculate

--- section4/Customer.cs
+++ section4/Customer.cs
@@ -2,6 +2,13 @@ using System;
 
 namespace Amazon
 {
+    public class RateCalculator
+    {
+        public int Calculate(Customer customer)
+        {
+            return 0;
+        }
+    }
     public class Customer
     {
         public int Id { get; set }
