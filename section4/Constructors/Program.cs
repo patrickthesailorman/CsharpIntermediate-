4e442f590fd77f9d6c2e9977083f98b91278ea3d
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-06 16:11:33 -0400

    Add Vehicle

--- section4/Constructors/Program.cs
+++ section4/Constructors/Program.cs
@@ -2,6 +2,13 @@
 
 namespace Constructors
 {
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized.");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
