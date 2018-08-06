ed49a88e321acb445f907b6374828bd676252e80
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-06 16:15:31 -0400

    Add Car.cs

---
+++ section4/Constructors/Car.cs
@@ -0,0 +1,10 @@
+namespace Constructors
+{
+    pubic class Car : Vehicle
+    {
+        public Car()
+        {
+            Console.WriteLine("Car is being initialized.");
+        }
+    }
+}
--- section4/Constructors/Program.cs
+++ section4/Constructors/Program.cs
@@ -2,13 +2,6 @@
 
 namespace Constructors
 {
-    pubic class Car : Vehicle
-    {
-        public Car()
-        {
-            Console.WriteLine("Car is being initialized.");
-        }
-    }
     class Program
     {
         static void Main(string[] args)
