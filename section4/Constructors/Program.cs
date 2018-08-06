946a61556b14cea051a4b486238c687dd13306a2
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-06 16:16:51 -0400

    Add new car()

--- section4/Constructors/Program.cs
+++ section4/Constructors/Program.cs
@@ -6,7 +6,8 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var car = new Car();
+           
         }
     }
 }
