20bb69cd9c01aa8a15ef85bac98de577958bae2d
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-08 20:34:20 -0400

    Add Text

--- section4/Upcasting/Program.cs
+++ section4/Upcasting/Program.cs
@@ -2,17 +2,10 @@
 
 namespace Upcasting
 {
-    public class Shape
+    public class Text : Shape
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public void Draw()
-        {
-            
-        }
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
     }
     class Program
     {
