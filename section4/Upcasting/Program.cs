c892bfcbfd541ef5fdd90aa1ff0732aaf5d5804e
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-08 21:02:02 -0400

    Add list.Add()

--- section4/Upcasting/Program.cs
+++ section4/Upcasting/Program.cs
@@ -8,7 +8,11 @@ namespace Upcasting
     {
         static void Main(string[] args)
         {
-            StreamReader reader = new StreamReader(new MemoryStream());
+            // StreamReader reader = new StreamReader(new MemoryStream());
+            var list  = new ArrayList();
+            list.Add(1);
+            list.Add("Mosh");
+            list.Add(new Text());
 
         }
     }
