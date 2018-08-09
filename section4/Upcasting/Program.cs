7b04ab339d89b829357a216f2d395698daedb4a8
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-08 21:06:27 -0400

    Add new Text

--- section4/Upcasting/Program.cs
+++ section4/Upcasting/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+
 
 namespace Upcasting
 {
@@ -8,13 +11,7 @@ namespace Upcasting
     {
         static void Main(string[] args)
         {
-            // StreamReader reader = new StreamReader(new MemoryStream());
-            var list  = new ArrayList();
-            list.Add(1);
-            list.Add("Mosh");
-            list.Add(new Text());
-
-            var anotherList = new List<Shape>();
+            Shape shape = new Text();
 
         }
     }
