1169c34a0cac4564211147c12457294140e08b4e
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-08 20:31:03 -0400

    Add Draw()

--- section4/Upcasting/Program.cs
+++ section4/Upcasting/Program.cs
@@ -6,6 +6,13 @@ namespace Upcasting
     {
         public int Width { get; set; }
         public int Height { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Draw()
+        {
+            
+        }
     }
     class Program
     {
