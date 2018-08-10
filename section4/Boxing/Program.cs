b397da37f9fea956a2b398b6c49c16ede0d23661
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-10 10:24:12 -0400

    Add var number

--- section4/Boxing/Program.cs
+++ section4/Boxing/Program.cs
@@ -11,6 +11,8 @@ namespace Boxing
             list.Add(1);
             list.Add("Mosh");
             list.Add(DateTime.Today);
+
+            var number = (int) list[1];
             
             // Console.WriteLine("Hello World!");
         }
