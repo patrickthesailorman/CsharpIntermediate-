7fd0cdf466421dd9ade84f0ea6e405b718209833
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-11 14:55:45 -0400

    Add method_Overriding

---
+++ section5/method_Overiding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace method_Overiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
