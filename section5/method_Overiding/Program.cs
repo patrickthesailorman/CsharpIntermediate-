5b0fc6f74b2281fc6780396786f50623df0ef24c
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-12 19:59:40 -0400

    Add Shape

--- section5/method_Overiding/Program.cs
+++ section5/method_Overiding/Program.cs
@@ -2,6 +2,11 @@
 
 namespace method_Overiding
 {
+    public class Shape
+    {
+        public int Width { get; set }
+        public int Height { get; set }
+    }
     class Program
     {
         static void Main(string[] args)
